4130d0c18a94b4468be97adbb6358267edcbf05e
Tengizovich <113883231+Tengizovich@users.noreply.github.com>
2023-09-02 13:08:29 +0300

    Create 1.js

---
+++ 1.js
@@ -0,0 +1,12 @@
+// Задание 1
+const isEven = (number) => {
+   if (number === 0) {
+      return `${number} – это ноль`;
+   } else if (number % 2 === 0) {
+      return `${number} – четное число`;
+   } else {
+      return `${number} – нечетное число`;
+   }
+}
+for (let i = 0; i <= 10; i++) {
+   console.log(isEven(i));
